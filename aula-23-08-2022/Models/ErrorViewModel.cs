a65de789f7c4d1c635508425e048c712c871940d
joao.msantos <joao.msantos@SJ212-29>
2022-08-23 07:42:02 -0300

    Add project files.

---
+++ aula-23-08-2022/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aula_23_08_2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
